388d888f9c65518967fc9ebc6c418d7e5478b6af
Raies1000 <raies1000@users.noreply.github.com>
2019-01-30 16:26:35 +0100

    Update files
    - Add javadoc
    - Minor warning fixes

--- src/test/java/org/jpacman/test/framework/model/BoardTileAtTest.java
+++ src/test/java/org/jpacman/test/framework/model/BoardTileAtTest.java
@@ -16,21 +16,30 @@ import org.junit.runners.Parameterized.Parameters;
 /**
  * Test the Board.tileAt method by means of a series of test cases with
  * Parameterized data.
- * 
+ *
  * @author Arie van Deursen, TU Delft, Feb 10, 2012
  */
 @RunWith(Parameterized.class)
 public class BoardTileAtTest {
 
-	private int startx, starty;
+    private int startx, starty;
 	private Direction dir;
 	private int nextx, nexty;
 	private Board board;
 
 	private static final int WIDTH = 10;
 	private static final int HEIGHT = 20;
+	private static final int MAGICPARAMETER = 3;
 
-	public BoardTileAtTest(int x, int y, Direction d, int nx, int ny) {
+	/**
+	 * @param x asf
+	 * @param y asf
+	 * @param d asf
+	 * @param nx asf
+	 * @param ny asf
+	 */
+	public BoardTileAtTest(final int x, final int y, final Direction d,
+	        final int nx, final int ny) {
 		startx = x;
 		starty = y;
 		dir = d;
@@ -39,6 +48,9 @@ public class BoardTileAtTest {
 		board = new Board(WIDTH, HEIGHT);
 	}
 
+	 /**
+     * askfas.
+     */
 	@Test
 	public void testTileAtDirection() {
 		Tile source = board.tileAt(startx, starty);
@@ -47,20 +59,24 @@ public class BoardTileAtTest {
 		assertEquals(desired, actual);
 	}
 
+	/**
+	 *
+	 * @return asd
+	 */
 	@Parameters
 	public static Collection<Object[]> data() {
 		Object[][] values = new Object[][] {
 				// x-axis boundaries, y random inpoints
 				// left boundary
-				{ 2, 2, Direction.UP, 2, 1 },
-				{ 2, 2, Direction.DOWN, 2, 3 },
-				{ 2, 2, Direction.LEFT, 1, 2 },
-				{ 2, 2, Direction.RIGHT, 3, 2 },
+				{2, 2, Direction.UP, 2, 1 },
+				{2, 2, Direction.DOWN, 2, MAGICPARAMETER },
+				{2, 2, Direction.LEFT, 1, 2 },
+				{2, 2, Direction.RIGHT, MAGICPARAMETER, 2 },
 				// worm holes
-				{ 0, 2, Direction.LEFT, WIDTH - 1, 2 },
-				{ WIDTH - 1, 2, Direction.RIGHT, 0, 2 },
-				{ 2, 0, Direction.UP, 2, HEIGHT - 1 },
-				{ 2, HEIGHT - 1, Direction.DOWN, 2, 0 } };
+				{0, 2, Direction.LEFT, WIDTH - 1, 2 },
+				{WIDTH - 1, 2, Direction.RIGHT, 0, 2 },
+				{2, 0, Direction.UP, 2, HEIGHT - 1 },
+				{2, HEIGHT - 1, Direction.DOWN, 2, 0 } };
 		return Arrays.asList(values);
 	}
 
